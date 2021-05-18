c679d8b2ed2cffdcbf960123dd2b2ce492f9d606
Owen Jeon <owen.jeon@gmail.com>
2021-05-18 12:56:36 +0900

    변수초기화

--- src/screens/detail/store/index.ts
+++ src/screens/detail/store/index.ts
@@ -1,10 +1,10 @@
 import SubwayStore from "./Subway";
 import { fetchItemDetail } from "../../../service";
 import { RootStore } from "../../../store";
-import { computed, makeObservable, observable } from "mobx";  
+import { computed, makeObservable, observable } from "mobx";
 
 export default class DetailScreenModel {
-  agentId?: string
+  agentId?: string = undefined;
   constructor(private rootStore: RootStore, private id: string) {
     makeObservable(this, {
       agentId: observable,
@@ -20,7 +20,7 @@ export default class DetailScreenModel {
     const { data } = await fetchItemDetail(this.id);
     this.itemStore.setItem(this.id, data.item);
     this.subwayStore.setItem(data.subways);
-    this.agentId = data.agent.owner.owner_user_no
+    this.agentId = data.agent.owner.owner_user_no;
 
     await this.agentStore.fetchAgent(data.agent.owner.owner_user_no);
   }
@@ -29,16 +29,22 @@ export default class DetailScreenModel {
     return this.rootStore.uiStore.isPyeong ? "제곱미터" : "평";
   }
 
-  get item(): {[key: string]: string} | undefined {
-    const item = this.itemStore.getItem(this.id)
-    if(!item) return undefined
-    return {...item, fullAddress: `${item.local1} ${item.local2} ${item.local3} ${item.jibunAddress}`}
+  get item(): { [key: string]: string } | undefined {
+    const item = this.itemStore.getItem(this.id);
+    if (!item) return undefined;
+    return {
+      ...item,
+      fullAddress: `${item.local1} ${item.local2} ${item.local3} ${item.jibunAddress}`
+    };
   }
 
-  get agent(): {[key: string]: string} | undefined {
-    if(!this.agentId) return undefined
-    const agent = this.agentStore.getItem(this.agentId)
-    if(!agent) return undefined
-    return {...agent, title: `중개사: ${agent.agent_name} (${agent.user_name})`}
+  get agent(): { [key: string]: string } | undefined {
+    if (!this.agentId) return undefined;
+    const agent = this.agentStore.getItem(this.agentId);
+    if (!agent) return undefined;
+    return {
+      ...agent,
+      title: `중개사: ${agent.agent_name} (${agent.user_name})`
+    };
   }
 }
